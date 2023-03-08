51a4d017b4e8f8776759753381328d1387c0437c
Brandon Britton <brandonbritton.dev@gmail.com>
2023-03-08 16:13:52 +1100

    refactor boilerplate

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import logo from '../assets/logo.svg';
+
+const Nav = () => {
+
+    return (
+        <nav>
+            <div>
+                <img src={ logo } alt='logo' />
+                <p>
+                    Brandon.
+                </p>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
