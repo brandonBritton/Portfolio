d7204db0f5b3656a66f3a1621d676ec44a1ca85b
Brandon Britton <brandonbritton.dev@gmail.com>
2023-03-13 18:12:02 +1100

    small update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Nav from './components/Nav';
 import './App.css';
 
 function App() {
+  
   return (
+    
     <div className="App">
-      <header className="App-header">
         <Nav />
-      </header>
     </div>
+  
   );
 }
 
