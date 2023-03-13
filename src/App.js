c995e27b0199e9b881acc660354541991346186e
Brandon Britton <brandonbritton.dev@gmail.com>
2023-03-13 18:34:12 +1100

    add hero section/update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Nav from './components/Nav';
+import Hero from './components/Hero';
 import './App.css';
 
 function App() {
-  
+
   return (
     
     <div className="App">
         <Nav />
+        <Hero />
     </div>
   
   );
---
+++ src/components/Hero.js
@@ -0,0 +1,24 @@
+const Hero = () => {
+
+    return (
+        <div>
+            <div>
+                {/* some graphic for hero section */}
+            </div>
+            <div>
+                {/* <h1>Hi there!</h1>
+                <h2>I’m Brandon,</h2> 
+                <h3>full-stack software developer</h3>
+                <p>
+                    I’m a software engineer specializing in building
+                    (and occasionally designing) exceptional digital experiences. Currently,
+                    I’m focused on building accessible, human-centered products.
+                </p>      */}
+            </div>
+        </div>
+        
+    );
+};
+
+
+export default Hero;
