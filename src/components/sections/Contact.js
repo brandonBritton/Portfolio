3cfb17473422d14c52b70bc2ebec1f4ebcaa3f96
Brandon Britton <brandonbritton.dev@gmail.com>
2023-05-02 10:52:39 +1000

    WIP

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,8 +1,18 @@
-const Contact = () => {
+import { email } from "config";
 
+const Contact = () => {
+    // placeholder text
     return (
         <div>
             <h1>Get in touch</h1>
+            <p>
+                Although I’m not currently looking for any new opportunities, my inbox is always open.
+                Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+            </p>
+
+            <a className="email-link" href={`mailto:${email}`}>
+                Say Hello
+            </a>
         </div>
         
     );
---
+++ src/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    email: "brandonbritton.dev@gmail.com",
+
+    socials: [
+        {
+            name: "GitHub",
+            url: "https://github.com/brandonBritton"
+        },
+        {
+            name: "LinkedIn",
+            url: "https://www.linkedin.com/in/brandon-britton"
+        }
+    ],
+
+    navLinks: [
+        {
+            name: 'About',
+            url: '/#about',
+        },
+        {
+            name: 'Experience',
+            url: '/#experience',
+        },
+        {
+            name: 'Contact',
+            url: '/#contact',
+        },
+    ]
+};
