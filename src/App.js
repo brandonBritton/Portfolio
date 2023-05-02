6181f29bdc21701cbf77307bff66b34e3e439b91
Brandon Britton <brandonbritton.dev@gmail.com>
2023-05-02 11:11:33 +1000

    refactor

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { Layout, Nav, Footer, Hero, About, Contact, Experience } from "components";
+import {
+  Layout,
+  Nav,
+  Footer,
+  Hero,
+  About,
+  Contact,
+  Experience,
+} from "components";
 import styles from "styles/App.module.scss";
 
-
 function App() {
-
-  	return (
-    	<div className={styles.App} data-theme="">
-			<Nav />
-			<Layout content={ <Hero /> }/>
-			<Layout content={ <About /> } />
-			<Layout content={ <Experience /> } />
-			<Layout content={ <Contact /> } />
-			<Footer />
-    	</div>     
-  	);
+  return (
+    <div className={styles.App} data-theme="">
+      <Nav />
+      <Layout content={<Hero />} />
+      <Layout content={<About />} />
+      <Layout content={<Experience />} />
+      <Layout content={<Contact />} />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
