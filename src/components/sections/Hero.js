0ea9dcd8294789bc4350d137b3f9d584976b51a9
Brandon Britton <brandonbritton.dev@gmail.com>
2023-05-10 12:59:14 +1000

    ui tweaking

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -4,19 +4,27 @@ import wave from "assets/waving-hand.png";
 const Hero = () => {
   return (
     <div className={styles.card}>
-      <h1>
-        <span className={styles.accent1}>Hi there!</span>
-        <img className={styles.wave} src={wave} alt="wave emoji" />
-      </h1>
-      <h2>
-        I’m <span className={styles.accent2}>Brandon Britton</span>
-      </h2>
-      <h3>full-stack software developer</h3>
-      <p>
-        I’m a software engineer specializing in building (and occasionally
-        designing) exceptional digital experiences. Currently, I’m focused on
-        building accessible, human-centered products.
-      </p>
+      <div className={styles.blobcont}>
+				<div className={`${styles.red} ${styles.blob}`}/>
+				<div className={`${styles.green} ${styles.blob}`}/>
+				<div className={`${styles.blue} ${styles.blob}`}/>
+      </div>
+			
+			<div className={styles.content}>
+        <h1>
+          <span className={styles.accent1}>Hi there!</span>
+          <img className={styles.wave} src={wave} alt="wave emoji" />
+        </h1>
+        <h2>
+          I’m <span className={styles.accent2}>Brandon Britton</span>
+        </h2>
+        <h3>full-stack software developer</h3>
+        <p>
+          I’m a software engineer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I’m focused on
+          building accessible, human-centered products.
+        </p>
+      </div>
     </div>
   );
 };
